8ec61ecbe64f5eeff28a67b508ed3540e9918362
Wessam Ibrahim <wessamfathi@gmail.com>
2024-05-21 18:39:31 +0200

    Solving structs, enums and strings

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -5,8 +5,6 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -30,6 +28,18 @@ fn create_order_template() -> Order {
     }
 }
 
+fn create_order(order_template: &Order) -> Order {
+    Order {
+        name: String::from("Hacker in Rust"),
+        year: order_template.year,
+        made_by_phone: order_template.made_by_phone,
+        made_by_mobile: order_template.made_by_mobile,
+        made_by_email: order_template.made_by_email,
+        item_number: order_template.item_number,
+        count: 1,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,7 +48,7 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = create_order(&order_template);
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
